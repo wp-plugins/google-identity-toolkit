f95123e91cb50a888aa41c41b41257a18fcd271a
easyrp <easyrp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-16 19:11:01 +0000

    fix the bug when web server is running on a port other than 80
    git-svn-id: https://plugins.svn.wordpress.org/google-identity-toolkit/trunk@519969 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- git/util/gitUtil.php
+++ git/util/gitUtil.php
@@ -31,7 +31,7 @@ class gitUtil {
     $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https://' : 'http://';
     $url .= $_SERVER['SERVER_NAME'];
     if ($_SERVER['SERVER_PORT'] != '80') {
-      $url .= $_SERVER['SERVER_PORT'];
+      $url .= ':' . $_SERVER['SERVER_PORT'];
     }
     $url .= $_SERVER["REQUEST_URI"];
     return $url;
